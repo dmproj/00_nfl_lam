eb7d7c7c52d3a68bf3d7d53ef9486eefaa22ee90
SY <seyav29@gmail.com>
2023-05-16 21:47:39 -0400

    .

--- routes/lists.js
+++ routes/lists.js
@@ -18,4 +18,50 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(200).json("LIST deleted successfuly");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("MOVIE CREATE you are not allowed");
+  }
+});
+
+//GET
+router.get("/:id", verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const genreQuery = req.query.genre;
+  let list = [];
+  if (req.user.isAdmin) {
+    try {
+      if (typeQuery) {
+        if (genreQuery) {
+          list = await List.aggregate([
+            { $sample: { size: 10 } },
+            { $match: { type: typeQuery, genre: genreQuery } },
+          ]);
+        } else {
+          list = await List.aggregate([
+            { $sample: { size: 10 } },
+            { $match: { type: typeQuery } },
+          ]);
+        }
+      } else {
+        list = await List.aggregate([{ $sample: { size: 10 } }]);
+      }
+      
+      res.status(200).json(list);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("MOVIE GET you are not allowed");
+  }
+});
+
 module.exports = router;
